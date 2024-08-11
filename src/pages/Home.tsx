4cb0c5a6fff0f9fdfa3504c55ed495667e35a4f7
P4ST4S <antoine.rospars@epitech.eu>
2024-08-11 12:32:52 +0200

    chore: Add styles and update layout for Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,21 @@
 import Typer from "../components/Typer";
+import styles from "../styles/pages/Home.module.css";
 
 function Home() {
   return (
-    <div>
+    <div className={styles.container}>
       HELLO!
-      <h1>I'm Antoine Rospars</h1>
-      <h2>
+      <h1 className={styles.title}>I'm Antoine Rospars</h1>
+      <h2 className={styles.description}>
         <Typer />
       </h2>
-      <div>
-        <button>HIRE ME</button>
-        <button>MY WORKS</button>
+      <div className={styles.buttonContainer}>
+        <button className={styles.buttonHire}>
+          <span className={styles.buttonTitle}>HIRE ME</span>
+        </button>
+        <button className={styles.buttonWork}>
+          <span className={styles.buttonTitle}>MY WORKS</span>
+        </button>
       </div>
     </div>
   );
