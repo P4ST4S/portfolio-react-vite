afe5165b796c454bc331e66d93b922dc07c3da29
P4ST4S <antoine.rospars@epitech.eu>
2024-09-08 18:09:45 +0200

    Add new pages and routes for About, Resume, and NotFound

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Skills from "./pages/Skills";
 import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Resume from "./pages/Resume";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -17,6 +20,9 @@ function App() {
             <Route path="/projects" element={<Projects />} />
             <Route path="/skills" element={<Skills />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </Layout>
       </Router>
